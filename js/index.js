817697a2632754c1273da9e36b53332085d14816
Collins <mutaic84@gmail.com>
2022-04-17 18:07:17 +0300

    display book titles on page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    const bookListContainer = document.getElementById("list-panel")
+    const bookContainer = document.getElementById("list")
+    const bookDataContainer = document.getElementById("show-panel")
+    function getBooks() {
+        fetch("http://localhost:3000/books")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                displayBookTitle(element)
+            });
+        })
+    }
+
+    function displayBookTitle(book) {
+        let li = document.createElement("li")
+        li.innerText = book.title
+        bookContainer.append(li)
+        li.addEventListener("click", () => {
+            displayBookInfo(book)
+        })
+    }
+
+    function displayBookInfo(book) {
+        console.log(book)
+    }
+    getBooks()
+});
